0b751c2ce11fee9a09b2cb2ddd1fbcaa19b03f98
Aryan Mahindra <62005619+Aryan181@users.noreply.github.com>
2023-03-11 18:39:27 -0800

    added confirmation animation

--- online/onlineSubmit.py
+++ online/onlineSubmit.py
@@ -14,24 +14,60 @@ def index():
     return render_template("index.html")
 
 
+
 @app.route("/submit", methods=["POST"])
 def submit():
     text = request.form.get("input-text")
     phone = request.form.get("input-phone")
+    clinic = request.form.get("input-clinics")
     reminder = request.form.get("input-reminder")
     times = request.form.getlist("input-time[]")
     phone = "+1" + phone
 
-    print(f"You entered text: {text}, phone: {phone}, reminder: {reminder}, and times: {times}")
+    print(f"You entered text: {text}, phone: {phone}, clinic: {clinic}, reminder: {reminder}, and times: {times}")
 
     # Remove duplicate times
     times = list(set(times))
 
-    for time in times:
-        record = {'Name': time, 'Values': phone}
-        airtable_client.insert(record)
-
-    return "Data received successfully!"
+    return """
+        <html>
+        <head>
+            <meta http-equiv="refresh" content="5; url=/">
+            <style>
+                #animation {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 100vh;
+                }
+                .checkmark {
+                    width: 70px;
+                    height: 70px;
+                    border-radius: 50%;
+                    display: inline-block;
+                    stroke-width: 3px;
+                    stroke: #4CAF50;
+                    stroke-dasharray: 166;
+                    stroke-dashoffset: 166;
+                    animation: stroke-checkmark 0.6s cubic-bezier(0.65, 0, 0.45, 1) forwards;
+                }
+                @keyframes stroke-checkmark {
+                    100% {
+                        stroke-dashoffset: 0;
+                    }
+                }
+            </style>
+        </head>
+        <body>
+            <div id="animation">
+                <svg class="checkmark" viewBox="0 0 52 52">
+                    <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/>
+                    <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+                </svg>
+            </div>
+        </body>
+        </html>
+        """
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
